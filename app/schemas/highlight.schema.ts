da5e929be1c72b02ee01a31098b9c0367b0deb10
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2025-08-01 00:25:40 +0200

    feat(schema): define Zod schema for highlights

---
+++ app/schemas/highlight.schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const highlightSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  cover_image_url: z.string().url(),
+  created_at: z.string(),
+  updated_at: z.string(),
+});
+
+export const highlightsSchema = z.array(highlightSchema);
+
+export type Highlight = z.infer<typeof highlightSchema>;
